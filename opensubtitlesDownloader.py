408156bcb5d5d74d020620517a4c0c5ca6f4ee1e
isayni <lennysep@protonmail.com>
2022-01-21 20:00:19 +0100

    error

--- opensubtitlesDownloader.py
+++ opensubtitlesDownloader.py
@@ -26,22 +26,23 @@ for i in range(1, len(sys.argv)):
 try:
     SEARCH
 except:
-    SEARCH = input()
+    sys.stderr.write('no query given')
+    exit()
 
 # searching the website for the chosen movie
 r = requests.get('https://www.opensubtitles.org/en/search2/sublanguageid-eng/moviename-' + SEARCH)
-bs = BeautifulSoup(r.text, "lxml")
-table = bs.find("table", {"id": "search_results"})
-row = table.find_all("tr")[1]
-TITLE = row.find("a").get_text().split("\n")[0]
-suffix = row.find("a").get("href")
+bs = BeautifulSoup(r.text, 'lxml')
+table = bs.find('table', {'id': 'search_results'})
+row = table.find_all('tr')[1]
+TITLE = row.find('a').get_text().split('\n')[0]
+suffix = row.find('a').get('href')
 
 print(TITLE)
 
 r = requests.get('https://www.opensubtitles.org/' + suffix)
 bs = BeautifulSoup(r.text, 'lxml')
-table = bs.find(id="search_results")
-rows = table.find_all("tr")[1:]
+table = bs.find(id='search_results')
+rows = table.find_all('tr')[1:]
 
 allOptions = []
 
@@ -52,7 +53,7 @@ for row in rows:
         a = cell.find('a')
         am = int(a.get_text().split('x')[0])
         if cell.find('span').get_text() == 'srt':
-            allOptions.append({"tag": a, "dws": am})
+            allOptions.append({'tag': a, 'dws': am})
     except:
         pass
 
@@ -60,10 +61,10 @@ for row in rows:
 allOptions = sorted(allOptions, key = lambda i: i['dws'], reverse=True)
 chosen = allOptions[NUMBER]
 
-print("picking the " + str(NUMBER + 1) + " most popular option with " + str(chosen["dws"]) + " downloads.")
+print('picking the ' + str(NUMBER + 1) + ' most popular option with ' + str(chosen['dws']) + ' downloads.')
 
 # downloading
-url = "https://www.opensubtitles.org" + chosen["tag"].get('href')
+url = 'https://www.opensubtitles.org' + chosen['tag'].get('href')
 wget.download(url)
 toRemove = []
 
@@ -76,6 +77,6 @@ for f in list(set(os.listdir(PATH)) - set(FILES)):
         toRemove += list(set(allFiles) - set(FILES))
 for f in toRemove:
     if f.endswith('.srt'):
-        os.rename(f, TITLE + " - ENG.srt")
+        os.rename(f, TITLE + ' - ENG.srt')
     else:
         os.remove(f)
